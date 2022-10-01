1f87fcca1a183be22f0f0c4b78234014a64ded5a
Lynn <griff.lynn8@gmail.com>
2022-10-01 11:28:14 -0700

    Skeleton for GamePage implemented

--- client/src/components/GamePage/GamePage.js
+++ client/src/components/GamePage/GamePage.js
@@ -1,8 +1,14 @@
+import Info from './Info.js';
+import Map from './Map.js';
+import Comments from './Comments.js';
+
 const GamePage = () => {
 
   return(
   <div>
-
+    <Info />
+    <Map />
+    <Comments />
   </div>
 }
 
